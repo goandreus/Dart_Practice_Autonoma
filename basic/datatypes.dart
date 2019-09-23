a4ff35d851465efb99497c5336d22e3d630025d0
goandreus <andreshtml5@gmail.com>
2019-09-23 15:40:53 -0500

    tipado de datos

---
+++ basic/datatypes.dart
@@ -0,0 +1,19 @@
+main() {
+  //Numeros
+  int a = 10;
+  double b = 10.5;
+
+  int c;
+
+  int x=10, y=20, z=30;
+
+  int _a; //privados
+
+  double $b =45.55;
+
+  print($b);
+  print(x);
+  print(y);
+  print(z);
+
+}
